4ef9bd1f864405fb1a11b4afe7d4acf730f2e3ee
DevEmmy <eolaosebikan60@gmail.com>
2023-03-25 00:33:40 +0100

    to 3

--- controllers/listingsControllers.js
+++ controllers/listingsControllers.js
@@ -6,7 +6,7 @@ const Category = require("../models/categories.model");
 
 const getAllListing = async (req, res) => {
     let { page, category, forRent } = req.query;
-    const limit = 6
+    const limit = 3
     category = await Category.findOne({ slug: category })
     const length = (await Listing.find({ category: category._id })).length
 
