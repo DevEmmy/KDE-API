37aba91f026ed32b496713c87e50ecefe0d062ad
DevEmmy <eolaosebikan60@gmail.com>
2023-03-26 00:46:52 +0100

    populate categories

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -56,7 +56,7 @@ const getAllCart = async (req, res)=>{
     const loggedUser = req.user;
 
     try{
-        let carts = await Cart.find({user: loggedUser}).populate("collectibles")
+        let carts = await Cart.find({user: loggedUser}).populate("collectibles").populate("categories").populate("collectibles.categories")
         res.json(carts)
     }
     catch(err){
