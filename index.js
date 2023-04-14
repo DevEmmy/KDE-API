31345347e9bf6742cab72c71505f3681bff5d4ce
DevEmmy <eolaosebikan60@gmail.com>
2023-04-14 23:17:11 +0100

    update socket

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ io.on("connection", (socket)=>{
       })
 
       socket.on("sendMessage", (m)=>{
-        console.log(m.messageContent)
+        console.log('here')
         var socketId = getUser(m.receiverID);
         // console.log(socketId)
         // console.log(users)
