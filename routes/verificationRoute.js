9bc79bf070b98c4c3c121e6867fe1f4e04f035c2
DevEmmy <eolaosebikan60@gmail.com>
2023-05-18 01:26:06 +0100

    fix bug

--- routes/verificationRoute.js
+++ routes/verificationRoute.js
@@ -1,7 +1,7 @@
 const { getVerificationStatus } = require("../controllers/verificationController");
 const requireLogin = require("../middlewares/requireLogin");
 
-const router = require("express").Router;
+const router = require("express").Router();
 
 router.get('/my-status', requireLogin ,getVerificationStatus)
 
