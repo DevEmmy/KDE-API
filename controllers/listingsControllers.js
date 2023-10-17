962e251083ced6e3424a0bfe1b6485ada81cbcba
Emmanuel Olaosebikan <eolaosebikan60@gmail.com>
2023-10-17 16:24:56 +0100

    Update listingsControllers.js

--- controllers/listingsControllers.js
+++ controllers/listingsControllers.js
@@ -7,7 +7,7 @@ const {detector, upscalerFunction} = require("./imageQualityDetector.js")
 
 const getAllListing = async (req, res) => {
     let { page, category, forRent } = req.query;
-    const limit = 12
+    const limit = 30
     category = await Category.findOne({ slug: category })
     const length = (await Listing.find({ category: category?._id,forRent: forRent || false })).length
 
