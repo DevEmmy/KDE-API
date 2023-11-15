f05346ae4f87b9c9ce8dd54e7c408e6114987504
Emmanuel Olaosebikan <eolaosebikan60@gmail.com>
2023-11-15 21:47:50 +0100

    reverse order of listings

--- controllers/listingsControllers.js
+++ controllers/listingsControllers.js
@@ -12,7 +12,7 @@ const getAllListing = async (req, res) => {
     const length = (await Listing.find({ category: category?._id,forRent: forRent || false })).length
 
     if(category){
-        await Listing.find({ category: category?._id, forRent: forRent || false }).populate("postedBy").populate("category").skip(((page || 1) - 1) * limit)
+        await Listing.find({ category: category?._id, forRent: forRent || false }).populate("postedBy").populate("category").skip(((page || 1) - 1) * limit).sort({createdAt: -1})
         .limit(limit)
         .then(resp => {
             // console.log(resp)
